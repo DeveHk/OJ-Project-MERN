2cc6e91dc508f2c35ed140ca1bd26ed50c0c37bc
deveHk <connect.to.hk@gmail.com>
2024-06-08 02:35:56 +0530

    fix:deployment: added vercel.json

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,16 @@ import authRouter from "./routes/authRouter.js";
 import testRouter from "./routes/test.js";
 import problemRouter from "./routes/problemRouter.js";
 import problemUserRouter from "./routes/problemUserRounter.js";
-import compilerRouter from "./routes/compilerRoutes.js";
 import userRouter from "./routes/UserRouter.js";
 import cors from "cors";
 const app = express();
 DBConnection();
-app.use(
+/**app.use(
   cors({
-    origin: process.env.CLIENT, // your frontend URL
+    origin: "https://oj-project-mern.vercel.app", //[process.env.CLIENT, process.env.RELEASE, process.env.PRODUCTION],
     credentials: true,
   })
-);
+); */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use("/auth", authRouter);
 app.use("/problem", problemRouter);
 app.use("/question", problemUserRouter);
-app.use("/compiler", compilerRouter);
 app.use("/user", userRouter);
 app.use("/", testRouter);
 
